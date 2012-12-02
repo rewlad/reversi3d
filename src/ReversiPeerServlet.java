6e8112d3614855ee8c553bb58d2a1b1c8b0c1632
Sergei Kirjanov <rewlad@gmail.com>
2012-12-02 04:05:11 +0200

    reversi multiplayer chan api

--- src/ReversiPeerServlet.java
+++ src/ReversiPeerServlet.java
@@ -1,16 +1,23 @@
 
 import java.io.IOException;
 import javax.servlet.http.*;
+import java.util.UUID;
+import com.google.appengine.api.channel.ChannelMessage;
+import com.google.appengine.api.channel.ChannelService;
+import com.google.appengine.api.channel.ChannelServiceFactory;
 
 public class ReversiPeerServlet extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
         throws IOException 
     {
-        String pi = req.getPathInfo();
-        ChannelService cs = ChannelServiceFactory.getChannelService();
+        String create = req.getParameter("create");
+        String peer = req.getParameter("peer");
+        String state = req.getParameter("state");
         resp.setContentType("text/plain");
-        resp.getWriter().write(cs.createChannel(pi));
+        ChannelService cs = ChannelServiceFactory.getChannelService();
+        if(create!=null) resp.getWriter().print(
+            create.equals("uuid") ? UUID.randomUUID() : cs.createChannel(create)
+        );
+        if(peer!=null) cs.sendMessage(new ChannelMessage(peer, state));
     }
 }
-
-//cs.sendMessage(new ChannelMessage(channelKey, getMessageString()));
